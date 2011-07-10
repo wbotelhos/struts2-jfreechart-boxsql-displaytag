865babf4d56644c0469e1803d79a5eac88bec603
Washington Botelho <wbotelhos@gmail.com>
2011-07-09 21:00:04 -0300

    Criado Action do Sale.

---
+++ src/com/bi/action/SaleAction.java
@@ -0,0 +1,36 @@
+package com.bi.action;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.bi.bean.Sale;
+import com.bi.dao.SaleDAO;
+
+public class SaleAction {
+
+	private SaleDAO dao = new SaleDAO();
+
+	private Map<String, Sale> salesMap;
+
+	public String list6MonthSales() {
+		salesMap = new LinkedHashMap<String, Sale>();
+
+		Collection<Sale> saleList = dao.getLastSixMonthsOfSales();
+
+		for (Sale sale : saleList) {
+			salesMap.put(sale.getMonth(), sale);
+		}
+
+		return "success";
+	}
+
+	public Map<String, Sale> getSalesMap() {
+		return salesMap;
+	}
+
+	public void setSalesMap(Map<String, Sale> salesMap) {
+		this.salesMap = salesMap;
+	}
+
+}
