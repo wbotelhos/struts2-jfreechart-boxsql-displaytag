f7b69d8cb0d29cbf38def842a0b1eebfb9d1f692
Washington Botelho <wbotelhos@gmail.com>
2011-07-10 01:22:58 -0300

    Adicionado campo date na tabela Sale do arquivo SQL.

--- src/com/bi/boxsql/sale.sql
+++ src/com/bi/boxsql/sale.sql
@@ -1,5 +1,6 @@
 CREATE TABLE sale (
 	month	varchar(3)				NOT NULL default '',
+	date	date					NOT NULL default '0000-00-00',
 	total	double					NOT NULL default '0',
 	seq		int(10)		unsigned	NOT NULL default '0',
 
